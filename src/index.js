c147d110b5b81dc7dbd2856bf0d6746f26dea978
Mythri6 <mythripeddindla430@gmail.com>
2024-11-11 11:41:27 +0530

    basic_create_react_app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import Mylistpage from './pages/mylistpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    <Mylistpage/>
   </React.StrictMode>
 );
 
---
+++ src/pages/mylistpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mylistpage() {
+  return (
+    <div>Mylistpage</div>
+  )
+}
+
+export default Mylistpage
